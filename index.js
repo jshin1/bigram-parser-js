08294b9cc3e75366783734cb58f7a9b1a78a1a15
Ji Shin <j2yshin@gmail.com>
2019-08-14 12:07:44 -0400

    Add function to iterate over each character to determine word

--- index.js
+++ index.js
@@ -5,8 +5,31 @@ const inStream = fs.createReadStream('sample.txt');
 const outStream = new stream();
 const rl = readline.createInterface(inStream, outStream);
 
+let words = []
+
 rl.on('line', (line) => {
-  console.log(typeof line);
+
+  let word = [0, 0]
+
+  if (words.length < 2) {
+
+    while (words.length < 2) {
+      while ((line[word[0]].charCodeAt() < 65 || line[word[0]].charCodeAt() > 122) && word[0] < line.length) {
+        word[0]++
+        word[1]++
+      }
+
+      if (line[word[0]].charCodeAt() >= 65 && line[word[0]].charCodeAt() <= 122 && word[0] < line.length) {
+        while ((line[word[1]].charCodeAt() >= 65 && line[word[1]].charCodeAt() <= 122) && word[1] < line.length) {
+          word[1]++
+        }
+
+        words.push(line.slice(word[0], word[1]))
+        word = [word[1], word[1]]
+      }
+    }
+    console.log(words);
+  }
 })
 
 rl.on('close', () => {
